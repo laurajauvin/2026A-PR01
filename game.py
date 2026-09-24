cc859f00047519c5e8804c1d4aa3d8e62c6712cb
Sarah <ssarahm289@gmail.com>
2026-09-24 14:19:33 -0400

    sauvegarde 1

--- game.py
+++ game.py
@@ -37,7 +37,15 @@ def move_doodle():
 
     # TODO : Gérez les déplacements gauche/droite et mettez à jour
     # simultanément la direction et l'image du Doodle.
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        doodle_dict["x"] -= DOODLE_SPEED
+        doodle_dict["direction"] = "left"
+        doodle_dict["image"] = doodle_left_img
 
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        doodle_dict["x"] += DOODLE_SPEED
+        doodle_dict["direction"] = "right"
+        doodle_dict["image"] = doodle_right_img
 
 
     # TODO : Implémentez le Screen Wrap pour qu'une partie du Doodle puisse
